97856df07f225858322dff14f0b96a6c7d24aae1
Olezha1852 <157027506+Viktoria-Nikolaevna@users.noreply.github.com>
2025-01-22 15:14:12 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Bechenka
+{
+    class Publicationn
+    {
+        public string AFtor { get; set; }
+        public string TEXAS { get; set; }
+        public Publicationn() { }
+        public Publicationn(string Aftor, string texa)
+        {
+            AFtor = Aftor;
+            TEXAS = texa;
+        }
+        public virtual void Show()
+        {
+            Console.WriteLine($"Публикация: Автор = {AFtor}, содержание = {TEXAS}");
+        }
+    }
+    class Statyaa : Publicationn
+    {
+        public string Shournal;
+        public int God_vupuska;
+        public Statyaa() { }
+        public Statyaa(string Aftor, string shournal, int god_vupuska, string texa) : base(Aftor, texa)
+        {
+            Shournal = shournal;
+            God_vupuska = god_vupuska;
+        }
+        public override void Show()
+        {
+            Console.WriteLine($"Статья: Автор - {AFtor}, журнал - {Shournal}, год выпуска - {God_vupuska}, содержание - {TEXAS}");
+        }
+    }
+    class Uchebnikk : Publicationn
+    {
+        public int Clas;
+        public Uchebnikk() { }
+        public Uchebnikk(string Aftor, int clas, string texa) : base(Aftor, texa)
+        {
+            Clas = clas;
+        }
+        public override void Show()
+        {
+            Console.WriteLine($"Учебник: Автор - {AFtor}, класс - {Clas}, содержание - {TEXAS}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Bechenka
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Publicationn> Inf = new List<Publicationn>();
+            Inf.Add(new Publicationn("Тузовский А.Ф.", "Системы управления знаниями на основе использования онтологических моделей и семантических методов."));
+            Inf.Add(new Publicationn("Чудинова М.В.", "Краткось юного Я"));
+            Inf.Add(new Statyaa("Викторов О.К.", "Атланты из Кемеровской области", 1997, "Интеграция распределённой и разнородной информации и данных организации."));
+            Inf.Add(new Statyaa("Чудинов Н.Б.", "Новости в областях IT", 2006, "Реализация проекта: 1984 в сети"));
+            Inf.Add(new Uchebnikk("Викторов О.К.", 11, "Объектно-ориентированное программирование. Учебное пособие для прикладного бакалавриата."));
+            Inf.Add(new Uchebnikk("Якубов В.В.", 9, "ДеЛюмпенизация математики"));
+            foreach (Publicationn Uchenie in Inf)
+            {
+                Uchenie.Show();
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
